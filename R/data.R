a31c4e6981666109a8cde414902353943221df1d
Laura Carroll <lmc297@cornell.edu>
2019-10-12 23:04:56 +0200

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,18 +1,24 @@
-#' bactaxR sample data: fastANI output
+#' bactaxR sample data
 #'
-#' A subset of taxonomic data from a
+#' Taxonomic and phylogenomic data from a
 #' study of 2,231 B. cereus group isolate genomes.
 #' Data incudes pairwise average nucleotide identity
 #' (ANI) values calculated between a subset of
-#' B. cereus group genomes (n = 36).
+#' B. cereus group genomes (n = 36), a phylogeny 
+#' of 2,231 B. cereus group isolates, and metadata 
+#' for 2,231 B. cereus group genomes (i.e., 
+#' Supplementary Table S1 in Carroll, Wiedmann, and 
+#' Kovac [2019]).
 #'
 #'
 #'
 #' @docType data
 #'
-#' @usage data(bactaxR_fastANI)
+#' @usage data(bactaxR_data)
 #'
-#' @format An object of class \code{"bactaxRObject"}; see \code{\link[bactaxR]{read.ANI}}.
+#' @format A list, where the first element is a bactaxRObject containing 
+#' pairwise ANI values between 36 B. cereus group genomes (i.e., fastANI 
+#' output), the second is a phylogeny of class phylo, and the third is a tibble of metadata.
 #'
 #' @keywords datasets
 #'
@@ -23,68 +29,11 @@
 #' bioRxiv 779199; doi: https://doi.org/10.1101/779199
 #'
 #' @examples
-#' data(bactaxR_fastANI)
-#' \donttest{plot.histogram <- ANI.histogram(bactaxRObject = bactaxR_fastANI, bindwidth = 0.001)}
+#' data(bactaxR_data)
+#' \donttest{plot.histogram <- ANI.histogram(bactaxRObject = bactaxR_data$ANI, bindwidth = 0.001)}
 #' \donttest{plot.histogram}
-"bactaxR_fastANI"
-
-
-
-#' bactaxR sample data: phylogeny
-#'
-#' Phylogenomic data from a
-#' study of 2,231 B. cereus group isolate genomes.
-#' Data incudes a phylogeny of 2,231 B. cereus
-#' group genomes.
-#'
-#'
-#' @docType data
-#'
-#' @usage data(bactaxR_phylo)
-#'
-#' @format An object of class phylo
-#'
-#' @keywords datasets
-#'
-#' @references Laura M. Carroll, Martin Wiedmann, and Jasna Kovac. 2019.
-#' Proposal of a taxonomic nomenclature for the Bacillus cereus group
-#' which reconciles genomic definitions of bacterial species
-#' with clinical and industrial phenotypes
-#' bioRxiv 779199; doi: https://doi.org/10.1101/779199
-#'
-#' @examples
-#' data(bactaxR_phylo)
-#' \donttest{plot.tree <- phylo.discrete_trait_OTU(phylo = bactaxR_phylo,
+#' \donttest{plot.tree <- phylo.discrete_trait_OTU(phylo = bactaxR_data$phylogeny,
 #' phylo_layout = "circular", tip_label_size = 0.5)}
 #' \donttest{plot.tree}
-"bactaxR_phylo"
-
-#' bactaxR sample data: metadata
-#'
-#' Metadata from a study of 2,231 B. cereus group
-#' isolate genomes. Data incudes a Microsoft Excel
-#' sheet with metadata for 2,231 B. cereus group genomes
-#'  (i.e., Supplementary Table S1 from Carroll, Wiedmann,
-#'  and Kovac [2019]).
-#'
-#'
-#' @docType data
-#'
-#' @usage data(bactaxR_metadata)
-#'
-#' @format An object of class tibble
-#'
-#' @keywords datasets
-#'
-#' @references Laura M. Carroll, Martin Wiedmann, and Jasna Kovac. 2019.
-#' Proposal of a taxonomic nomenclature for the Bacillus cereus group
-#' which reconciles genomic definitions of bacterial species
-#' with clinical and industrial phenotypes
-#' bioRxiv 779199; doi: https://doi.org/10.1101/779199
-#'
-#' @examples
-#' data(bactaxR_metadata)
-#' \donttest{head(bactaxR_metadata)}
-"bactaxR_metadata"
-
-
+#' \donttest{head(bactaxR_data$metadata)}
+"bactaxR_data"
