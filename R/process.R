a86f42125cdd16035048659f88ce6a986a490acf
Laura Carroll <lmc297@cornell.edu>
2019-10-13 17:32:59 +0200

    Update process.R

--- R/process.R
+++ R/process.R
@@ -7,5 +7,5 @@ bactaxR_metadata <- read_excel("data/sup_table_s1_genomes_ani.xlsx", skip = 1)
 bactaxR_data <- list(phylogeny = bactaxR_phylo, metadata = bactaxR_metadata)# ANI = bactaxR_fastANI, 
 print(length(bactaxR_data))
 print("bactaxR data loaded.")
-devtools::use_data(bactaxR_data, overwrite = T)
+devtools::use_data(bactaxR_phylo, bactaxR_metadata, overwrite = T)
 #usethis::use_data(bactaxR_data, overwrite = T)
