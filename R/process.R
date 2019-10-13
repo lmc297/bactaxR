5d5295b759c884ed0fea6a2216cbda63328c27e8
Laura Carroll <lmc297@cornell.edu>
2019-10-13 18:08:41 +0200

    Update process.R

--- R/process.R
+++ R/process.R
@@ -1,9 +1,9 @@
 # load sample fastANI data
 #bactaxR_fastANI <- read.ANI(file = "data/bactaxR_fastani_output.txt")
 # read phylogeny
-bactaxR_phylo <- read.newick(file = "data/bactaxR_phylogeny.nwk")
+bactaxR_phylo <- read.newick(file = "data-raw/bactaxR_phylogeny.nwk")
 # read metadata
-bactaxR_metadata <- read_excel("data/sup_table_s1_genomes_ani.xlsx", skip = 1)
+bactaxR_metadata <- read_excel("data-raw/sup_table_s1_genomes_ani.xlsx", skip = 1)
 bactaxR_data <- list(phylogeny = bactaxR_phylo, metadata = bactaxR_metadata)# ANI = bactaxR_fastANI, 
 print(bactaxR_data)
 print(length(bactaxR_data))
